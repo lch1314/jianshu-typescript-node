00279ec791d53a1d587e806fdd5ee9b821e01f71
liuchenghu <786973980@qq.com>
2019-06-06 15:48:22 +0800

    首页组件的拆分

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,10 @@ import { GlobalStyle } from './gloabl-style';
 import { GlobalIconStyle } from './statics/iconfont/iconfont';
 import Header from './common/header/index';
 import { Provider } from 'react-redux';
+import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store';
+import Home from './pages/home';
+import Detail from './pages/detail';
 
 
 function App() {
@@ -12,6 +15,10 @@ function App() {
       <GlobalStyle />
       <GlobalIconStyle />
       <Header />
+      <BrowserRouter>
+        <Route path="/" exact component={Home}></Route>
+        <Route path="/detail" exact component={Detail}></Route>
+      </BrowserRouter>
     </Provider>
   );
 }
