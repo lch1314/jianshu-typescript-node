53bc559dbf04d218646bb98f06b16dac65aad850
liuchenghu <786973980@qq.com>
2019-06-11 16:23:33 +0800

    登录页面布局

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import store from './store';
 import Home from './pages/home';
 import Detail from './pages/detail';
+import Login from './pages/login';
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <BrowserRouter>
         <Header />
         <Route path="/" exact component={Home}></Route>
+        <Route path="/login" exact component={Login}></Route>
         <Route path="/detail" exact component={Detail}></Route>
       </BrowserRouter>
     </Provider>
--- client/src/pages/detail/index.js
+++ client/src/pages/detail/index.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
+import { DetailWrapper, Header } from './style';
 
 class Detail extends Component {
     render () {
         return (
-            <div>Detail</div>
+            <DetailWrapper>
+                <Header>“我30岁，刚离职，才发现自己什么都不会”</Header>
+            </DetailWrapper>
         )
     }
 }
